a8e4f1c5c3ae0808b8eebe262dc6192d87914441
Madhu badam <badammadhu12@gmail.com>
2026-09-06 15:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.64%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                nums[mid]=nums[low];
+                nums[low]=0;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                nums[mid]=nums[high];
+                nums[high]=2;
+                high--;
+            }
+        }
+        //use dutch flag algorithm to solve this 
+        
+    }
+}
